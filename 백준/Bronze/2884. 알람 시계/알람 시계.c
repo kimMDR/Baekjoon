1ef025da3e8d93b9e1d74b8e1f61dfbc6163dd47
kimMDR <smilekm577@gmail.com>
2026-01-09 12:51:11 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int H, M;
+    scanf("%d %d", &H, &M);
+    if(M>=45) {
+        M-=45;
+        printf("%d %d", H, M);
+    }
+    else {
+        M+=15;
+        if(H == 0) {
+            H = 23;
+        printf("%d %d", H, M);
+        }
+        else {
+            H-=1;
+        printf("%d %d", H, M);
+        }
+    }
+    return 0;
+}
